ba91e1cc24872df4cdfc315217be8ae35dda5478
Juan Salvatore <juansalvatore@live.com.ar>
2017-12-19 03:35:34 -0300

    Add notes require

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
 console.log('starting app')
+
+const _ = require('lodash')
+const fs = require('fs')
+
+const notes = require('./notes')
---
+++ notes.js
@@ -0,0 +1,6 @@
+console.log('Starting notes')
+
+module.exports.addNote = () => {
+  console.log('add note')
+  return 'new note'
+}
